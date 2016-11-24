d968889c61b8fee6bb1dd57defc35aa571b93b4e
kamazs <martins.ceplis@gmail.com>
2016-11-24 20:13:52 +0200

    Removed unnecessary duplicates after folder structure reorganization

--- src/main.js
+++
@@ -1,9 +0,0 @@
-require("./styles/main.scss");
-import axios from "axios";
-
-document.write("Nu nez.");
-
-axios.post("http://localhost:3001/create").then( res=>{
-    document.write("POST Response: ", res.toString());
-    console.log("POST Response: ", res);
-});
